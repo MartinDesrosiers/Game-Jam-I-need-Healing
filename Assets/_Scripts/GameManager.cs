7f5b9ce2a0018421f80ea5cd3a516e2e789ab3f8
MartinDesrosiers <martindesrosiers1234@gmail.com>
2018-01-09 13:59:20 -0500

    Initial commit

---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private static GameManager m_instance;
+    public GameObject m_player;
+    public GameObject m_enemie;
+
+    GameObject[] players;
+    private GameManager() { }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (m_instance == null)
+                m_instance = new GameManager();
+            return m_instance;
+        }
+    }
+    private void Start()
+    {
+        players = new GameObject[3];
+        players[0] = Instantiate(m_player, new Vector3(0f, 1f, 0f), Quaternion.identity) as GameObject;
+        players[1] = Instantiate(m_player, new Vector3(2f, 1f, 0f), Quaternion.identity) as GameObject;
+        players[2] = Instantiate(m_player, new Vector3(4f, 1f, 0f), Quaternion.identity) as GameObject;
+        players[0].GetComponent<PlayerControler>().Init('1');
+        players[1].GetComponent<PlayerControler>().Init('2');
+        players[2].GetComponent<PlayerControler>().Init('3');
+    }
+}
---
+++ Assets/_Scripts/PlayerControler.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+    public float speed;
+    public char playerNumber;
+    // Use this for initialization
+    public void Init(char number)
+    {
+        playerNumber = number;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        switch (playerNumber)
+        {
+            case '1':
+                if (Input.GetKey("a") && !Input.GetKey("d"))
+                {
+                    transform.Translate(Vector3.left * speed * Time.deltaTime);
+                }
+                if (Input.GetKey("d") && !Input.GetKey("a"))
+                {
+                    transform.Translate(Vector3.right * speed * Time.deltaTime);
+                }
+                if (Input.GetKey("s") && !Input.GetKey("w"))
+                {
+                    transform.Translate(Vector3.back * speed * Time.deltaTime);
+                }
+                if (Input.GetKey("w") && !Input.GetKey("s"))
+                {
+                    transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                }
+                break;
+            case '2':
+                if (Input.GetKey("left") && !Input.GetKey("right"))
+                {
+                    transform.Translate(Vector3.left * speed * Time.deltaTime);
+                }
+                if (Input.GetKey("right") && !Input.GetKey("left"))
+                {
+                    transform.Translate(Vector3.right * speed * Time.deltaTime);
+                }
+                if (Input.GetKey("down") && !Input.GetKey("up"))
+                {
+                    transform.Translate(Vector3.back * speed * Time.deltaTime);
+                }
+                if (Input.GetKey("up") && !Input.GetKey("down"))
+                {
+                    transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                }
+                break;
+            case '3':
+                if (Input.GetKey("h") && !Input.GetKey("k"))
+                {
+                    transform.Translate(Vector3.left * speed * Time.deltaTime);
+                }
+                if (Input.GetKey("k") && !Input.GetKey("h"))
+                {
+                    transform.Translate(Vector3.right * speed * Time.deltaTime);
+                }
+                if (Input.GetKey("j") && !Input.GetKey("u"))
+                {
+                    transform.Translate(Vector3.back * speed * Time.deltaTime);
+                }
+                if (Input.GetKey("u") && !Input.GetKey("j"))
+                {
+                    transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                }
+                break;
+        }
+    }
+}
